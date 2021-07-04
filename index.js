917aa36e5bfa8c2f5e27ec8b3b23c9fc85f02a0a
Jason McCandless <jhmccandless1105@gmail.com>
2021-07-03 19:34:41 -0500

    got the layout of the first page, really rough but works to contain the information and is functional to the new food page

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ app.set("view engine", "html");
 app.use("/public", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
+app.get("/dashboard", async (req, res) => {
   const results = await db.query(`SELECT * FROM food_items`);
-  res.render("homepage.html", { locals: { results } });
+  console.log(results);
+  res.render("homepage", { locals: { results } });
+});
+
+app.get("/food_input", (req, res) => {
+  res.render("add-food");
 });
 
 const PORT = process.env.PORT || 3785;
