8bb03ff982af5091890cb7b8f66ff852cc7d8e17
Jason McCandless <jhmccandless1105@gmail.com>
2021-07-04 18:05:05 -0500

    working on the second part of the app, adding vs just using

--- index.js
+++ index.js
@@ -15,14 +15,35 @@ app.set("view engine", "html");
 app.use("/public", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+let foodInfo;
+
 app.get("/dashboard", async (req, res) => {
   const results = await db.query(`SELECT * FROM food_items`);
   console.log(results);
   res.render("homepage", { locals: { results } });
 });
 
+app.post("/food_input", (req, res) => {
+  foodInfo = req.body;
+  console.log(foodInfo);
+  //   db.none(
+  //     `INSERT INTO kcalorie (name, address, category) VALUES ('${foodInfo.food}', '${foodInfo.calories}', '${foodInfo.meal}', '${foodInfo.date}', '${foodInfo.time}')`
+  //   );
+  res.redirect(`/food_input/confirmation`);
+});
+
 app.get("/food_input", (req, res) => {
   res.render("add-food");
+});
+
+app.get("/food_input/confirmation", (req, res) => {
+  // render ability to get the info from the table to compare, ability to choose which one with a checkbox?
+  res.render("confirm-food", { locals: { foodInfo } });
+});
+
+app.post("/food_input/confirmation", (req, res) => {
+  // need a submit button,
+  res.redirect(`/dashboard`);
 });
 
 const PORT = process.env.PORT || 3785;
