f4f9a5b58ce45b2e68d1ada68ef7f01ce9f98b96
Jason McCandless <jhmccandless1105@gmail.com>
2021-07-08 20:14:56 -0500

    getting food input up to snuff

--- public/frontend.js
+++ public/frontend.js
@@ -1,6 +1,15 @@
+"use strict";
+
 window.onload = displayClock();
 function displayClock() {
-  let display = new Date();
+  let display = new Date().toLocaleString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+  });
   document.querySelector("#date-text").innerHTML = display;
   setTimeout(displayClock, 1000);
 }
