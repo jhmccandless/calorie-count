b3502a8dfa409c2301077cc369a1130ac1b6b34c
Jason McCandless <jhmccandless1105@gmail.com>
2021-07-09 22:40:49 -0500

    got the layout desired for the input page

--- public/frontend.js
+++ public/frontend.js
@@ -1,15 +1,54 @@
 "use strict";
 
-window.onload = displayClock();
-function displayClock() {
-  let display = new Date().toLocaleString("en-US", {
-    weekday: "long",
-    day: "numeric",
+if (document.querySelector("#date-text")) {
+  window.onload = displayClock();
+  function displayClock() {
+    let display = new Date().toLocaleString("en-US", {
+      weekday: "long",
+      day: "numeric",
+      year: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+    });
+    document.querySelector("#date-text").innerHTML = display;
+    setTimeout(displayClock, 1000);
+  }
+}
+
+if (document.querySelector("#food-form-div")) {
+  let currentYear = new Date().toLocaleString("en-US", {
     year: "numeric",
-    month: "long",
-    hour: "numeric",
-    minute: "numeric",
   });
-  document.querySelector("#date-text").innerHTML = display;
-  setTimeout(displayClock, 1000);
+  for (let i = 1; i <= 12; i++) {
+    document.querySelector(
+      "#user_input_month"
+    ).innerHTML += `<option value="0">${i}</option>`;
+  }
+  for (let i = 1; i <= 31; i++) {
+    document.querySelector(
+      "#user_input_day"
+    ).innerHTML += `<option value="0">${i}</option>`;
+  }
+  for (let i = 2019; i <= currentYear; i++) {
+    document.querySelector(
+      "#user_input_year"
+    ).innerHTML += `<option value="0">${i}</option>`;
+  }
+  for (let i = 1; i <= 12; i++) {
+    document.querySelector(
+      "#user_input_hour"
+    ).innerHTML += `<option value="0">${i}</option>`;
+  }
+  for (let i = 0; i <= 59; i++) {
+    document.querySelector(
+      "#user_input_minute"
+    ).innerHTML += `<option value="0">${i}</option>`;
+  }
+  document.querySelector(
+    "#user_input_ap"
+  ).innerHTML += `<option value="0">am</option>`;
+  document.querySelector(
+    "#user_input_ap"
+  ).innerHTML += `<option value="0">pm</option>`;
 }
